100e46911a3d7b6c06b7cdafa56485ac69ca619a
Mithun Bharadwaj <mithunbharadwaj02@gmail.com>
2019-08-14 22:22:43 -0400

    Adding input, output and source files

---
+++ src/vgg_model.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+The reference for this code is credited to the MatConvNet team and is used to load the parameters of the VGG19 model 
+
+"""
+from scipy.io import loadmat
+import tensorflow as tf
+import numpy as np
+
+image_height = 300
+image_width = 400
+color_channels = 3
+def load_vgg_model(path):
+    vgg = loadmat(path)
+    vgg_layers = vgg['layers']
+    
+    def _weights(layer, expected_layer_name): 
+        """
+        Return the weights and bias of a paricular layer
+        """
+        wb = vgg_layers[0][layer][0][0][0]
+        W = wb[0][0]
+        b = wb[0][1]
+        layer_name = vgg_layers[0][layer][0][0][3][0]
+        assert layer_name == expected_layer_name
+        return W, b
+          
+    def _conv2d(prev_layer, layer, layer_name):
+        """
+        Return the Conv2D layer using the weights, biases from the VGG
+        model at 'layer'.
+        """
+        W, b = _weights(layer, layer_name)
+        W = tf.constant(W)
+        b = tf.constant(np.reshape(b, (b.size)))
+        return tf.nn.conv2d(prev_layer, filter=W, strides=[1, 1, 1, 1], padding='SAME') + b
+    
+    def _relu(conv2d_layer):
+        """
+        Return the RELU function wrapped over a TensorFlow layer. Expects a
+        Conv2d layer input.
+        """
+        return tf.nn.relu(conv2d_layer)
+      
+    def _conv2d_relu(prev_layer, layer, layer_name):
+        """
+        Return the Conv2D + RELU layer using the weights, biases from the VGG
+        model at 'layer'.
+        """
+        return _relu(_conv2d(prev_layer, layer, layer_name))
+    
+    def _avgpool(prev_layer):
+        """
+        Return the AveragePooling layer.
+        """
+        return tf.nn.avg_pool(prev_layer, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    
+    # Constructs the graph model.
+    graph = {}
+    graph['input']   = tf.Variable(np.zeros((1, image_height, image_width, color_channels)), dtype = 'float32')
+    graph['conv1_1']  = _conv2d_relu(graph['input'], 0, 'conv1_1')
+    graph['conv1_2']  = _conv2d_relu(graph['conv1_1'], 2, 'conv1_2')
+    graph['avgpool1'] = _avgpool(graph['conv1_2'])
+    graph['conv2_1']  = _conv2d_relu(graph['avgpool1'], 5, 'conv2_1')
+    graph['conv2_2']  = _conv2d_relu(graph['conv2_1'], 7, 'conv2_2')
+    graph['avgpool2'] = _avgpool(graph['conv2_2'])
+    graph['conv3_1']  = _conv2d_relu(graph['avgpool2'], 10, 'conv3_1')
+    graph['conv3_2']  = _conv2d_relu(graph['conv3_1'], 12, 'conv3_2')
+    graph['conv3_3']  = _conv2d_relu(graph['conv3_2'], 14, 'conv3_3')
+    graph['conv3_4']  = _conv2d_relu(graph['conv3_3'], 16, 'conv3_4')
+    graph['avgpool3'] = _avgpool(graph['conv3_4'])
+    graph['conv4_1']  = _conv2d_relu(graph['avgpool3'], 19, 'conv4_1')
+    graph['conv4_2']  = _conv2d_relu(graph['conv4_1'], 21, 'conv4_2')
+    graph['conv4_3']  = _conv2d_relu(graph['conv4_2'], 23, 'conv4_3')
+    graph['conv4_4']  = _conv2d_relu(graph['conv4_3'], 25, 'conv4_4')
+    graph['avgpool4'] = _avgpool(graph['conv4_4'])
+    graph['conv5_1']  = _conv2d_relu(graph['avgpool4'], 28, 'conv5_1')
+    graph['conv5_2']  = _conv2d_relu(graph['conv5_1'], 30, 'conv5_2')
+    graph['conv5_3']  = _conv2d_relu(graph['conv5_2'], 32, 'conv5_3')
+    graph['conv5_4']  = _conv2d_relu(graph['conv5_3'], 34, 'conv5_4')
+    graph['avgpool5'] = _avgpool(graph['conv5_4'])
+    
+    return graph
